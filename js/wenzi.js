24c2cef13b1be701b0722903eea19482aba89d57
qvchuang <qvchuang@126.com>
2019-11-27 21:52:02 +0800

    Site updated: 2019-11-27 21:51:54

--- js/wenzi.js
+++ js/wenzi.js
@@ -1,3 +1,3 @@
-// build time:Tue Nov 26 2019 22:17:16 GMT+0800 (GMT+08:00)
+// build time:Wed Nov 27 2019 21:50:38 GMT+0800 (GMT+08:00)
 var a_idx=0;jQuery(document).ready(function(a){a("body").click(function(o){var n=new Array("❤富强❤","❤民主❤","❤文明❤","❤和谐❤","❤自由❤","❤平等❤","❤公正❤","❤法治❤","❤爱国❤","❤敬业❤","❤诚信❤","❤友善❤");var t=a("<span></span>").text(n[a_idx]);a_idx=(a_idx+1)%n.length;var e=o.pageX,d=o.pageY;t.css({"z-index":1e69,top:d-20,left:e,position:"absolute","font-weight":"bold",color:"rgb("+~~(255*Math.random())+","+~~(255*Math.random())+","+~~(255*Math.random())+")"});a("body").append(t);t.animate({top:d-180,opacity:0},1500,function(){t.remove()})})});
 //rebuild by neat 
