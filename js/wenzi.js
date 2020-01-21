2d6bd688ff822752c1cebf22890fcc8af30ae5f6
qvchuang <qvchuang@126.com>
2020-01-22 00:07:20 +0800

    Site updated: 2020-01-22 00:07:09

--- js/wenzi.js
+++ js/wenzi.js
@@ -1,3 +1,3 @@
-// build time:Sat Jan 18 2020 17:42:50 GMT+0800 (GMT+08:00)
+// build time:Wed Jan 22 2020 00:07:02 GMT+0800 (GMT+08:00)
 var a_idx=0;jQuery(document).ready(function(a){a("body").click(function(o){var n=new Array("❤富强❤","❤民主❤","❤文明❤","❤和谐❤","❤自由❤","❤平等❤","❤公正❤","❤法治❤","❤爱国❤","❤敬业❤","❤诚信❤","❤友善❤");var t=a("<span></span>").text(n[a_idx]);a_idx=(a_idx+1)%n.length;var e=o.pageX,d=o.pageY;t.css({"z-index":1e69,top:d-20,left:e,position:"absolute","font-weight":"bold",color:"rgb("+~~(255*Math.random())+","+~~(255*Math.random())+","+~~(255*Math.random())+")"});a("body").append(t);t.animate({top:d-180,opacity:0},1500,function(){t.remove()})})});
 //rebuild by neat 
